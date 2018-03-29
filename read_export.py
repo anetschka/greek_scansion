588e2e4d44dc27b572d2d36c1ff3f1b7ba38a740
anetschka <ak47schumann@gmail.com>
2018-03-29 16:45:28 +0200

    added selector class

--- read_export.py
+++ read_export.py
@@ -1,10 +1,47 @@
 import re
 import sys
 import codecs
+#for random number generation
+from random import randint
 #for syllabification
 from greek_accentuation.syllabify import syllabify, display_word
 
 ####CLASS DEFINITIONS####
+
+class selector(object):
+	
+	def __init__(self):
+		#this dictionary contains the total verse count for each work in the Homer corpus
+		self.works = {0: 15875, 1: 12150, 2: 1066, 3: 840, 4: 487, 5: 1835, 6: 2344}
+		#this dictionary contains the minimum sentenceid for each work in the Homer corpus
+		self.min = {0: 1, 1: 15874, 2: 28026, 3: 29092, 4: 29932, 5: 30419, 6: 32254}
+		#this dictionary contains the maximum sentenceid for each work in the Homer corpus
+		self.max = {0: 15875, 1: 28025, 2: 29091, 3: 29931, 4: 30418, 5: 32253, 6: 34597}
+		#dictionary to avoid duplicate selection
+		self.selected = {}
+	
+	def select(self, lines, level):
+		#return value
+		resultlines = []
+		
+		#update number of verses to be selected
+		for work in self.works:
+			count = round((self.works[work]/100)*level, 0)
+			self.works[work] = count
+
+		#select correct number of sentences
+		for work in self.works:
+			while self.works[work] > 0:
+				sentence = randint(self.min[work], self.max[work])
+				#check if this has already been selected
+				if sentence not in self.selected:
+					line = lines[sentence]
+					self.works[work] = self.works[work]-1
+					resultlines.append(line)
+					update = {sentence : 1}
+					self.selected.update(update)
+				
+		return resultlines
 
 class preprocessor(object):
 
@@ -118,15 +155,17 @@ class preprocessor(object):
 			
 		return resultsent.rstrip(' ')
 	
-class selector(object):
-	pass
-	
 ####MAIN PROGRAM####	
 	
 infile = codecs.open(sys.argv[1], "r", "utf-8")
 outfile = codecs.open(sys.argv[2], "w", "utf-8")
 
 lines = infile.readlines()
+
+#get a verse selector: use this to select and process a random subset of verses
+#sel = selector()
+#selection = sel.select(lines, 1)
+#print(selection)
 
 #get a preprocessor
 prep = preprocessor()
